29627dd719361ad5e77aa33570c6e250e508d26b
Kim Burgess <kim@acaprojects.com>
2017-07-12 11:56:38 +1000

    Fix incorrect import path for search plugin

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 // Setup docsify for client side md rendering
 import './docsify-conf';
 import 'docsify';
-import 'docsify/plugins/search';
+import 'docsify/lib/plugins/search';
 
 // Prism for readable code snippets
 
